6c444a260efbdee27bb4c3f12421fecea4dab75f
Felipe 0liveira <felpd3v@gmail.com>
2020-07-20 16:25:50 -0300

    Enhancements + Bugfix
    - WIP Contact
    - Prettify Script
    - Layout Fix Home
    - Session Validate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CodepenComponent } from './components/codepen/codepen.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, CodepenComponent, BlogComponent],
+  declarations: [
+    AppComponent,
+    CodepenComponent,
+    BlogComponent,
+    ContactComponent,
+  ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
