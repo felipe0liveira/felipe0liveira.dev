667aa0dabb6a7f9c872c42ae65b44fabca981614
Felipe Oliveira <felpd3v@gmail.com>
2020-08-12 10:55:43 -0300

    ArrayService with Shuffle Method

---
+++ src/app/services/utils/array.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ArrayService {
+  constructor() {}
+
+  shuffle(array: any[]) {
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+}
