9670f26d8ba3af55c050995d577e489ca96bc3c1
felipe0liveira <felpd3v@gmail.com>
2021-02-10 16:35:23 -0300

    feat(): Creates the GitRepositoriesComponent

---
+++ src/components/git-repositories/index.js
@@ -0,0 +1,28 @@
+import Window from '../window'
+import styles from './index.module.css'
+
+const openRepository = ({ url }) => {
+  window.open(url, '_blank')
+}
+
+const GitRepositories = ({ repos }) => {
+  return (
+    <>
+      <Window title='Repositórios' icon='github'>
+        {repos.map((repo) => (
+          <button
+            type='button'
+            className={styles.repo}
+            onClick={() => openRepository(repo)}
+            key={repo.id}
+          >
+            <p>{repo.name}</p>
+            <small>{repo.description}</small>
+          </button>
+        ))}
+      </Window>
+    </>
+  )
+}
+
+export default GitRepositories
