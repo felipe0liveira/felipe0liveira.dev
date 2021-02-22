2fe29613d4abe1dafc3303227cf006921173f5de
felipe0liveira <felpd3v@gmail.com>
2021-02-22 14:47:17 -0300

    feat(): Removes some props from Feed response

--- src/services/feed.service.js
+++ src/services/feed.service.js
@@ -9,10 +9,7 @@ const getFeed = async () => {
   postsDevTo = postsDevTo.map((item, index) => ({
     id: index,
     title: item.title[0],
-    author: item.author[0],
-    pubDate: item.pubDate[0],
     url: item.link[0],
-    description: item.description[0],
     category: item.category,
   }))
 
