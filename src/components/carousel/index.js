79e0fd45d0564a9cb48e065c7aa20919a0d5e5a6
felipe0liveira <felpd3v@gmail.com>
2021-02-22 15:33:58 -0300

    feat(): Creates Carousel Component

---
+++ src/components/carousel/index.js
@@ -0,0 +1,16 @@
+import './_mobile.scss'
+
+const Carousel = ({ children, ...pageProps }) => {
+  const { title } = pageProps
+
+  return (
+    <>
+      <div className='container'>
+        {title && <h2 className='title'>{title}</h2>}
+        <div className='carousel'>{children}</div>
+      </div>
+    </>
+  )
+}
+
+export default Carousel
