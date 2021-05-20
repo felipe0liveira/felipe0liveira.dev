0243077862a4cea52e1d99055573ee9c4cd8ea6b
felipe0liveira <felpd3v@gmail.com>
2021-05-20 11:38:37 -0300

    feat(): Adds an error handling for img on load

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,12 +1,18 @@
+import { useState } from 'react'
 import './styles.scss'
 
 const Card = ({ children, ...pageProps }) => {
   const { title, url, center, image } = pageProps
+  const [imageVisibility, setImageVisibility] = useState(true)
 
   const goTo = (url) => {
     if (url) {
       window.open(url, '_blank')
     }
+  }
+
+  const imgSrcErrorHandler = (error) => {
+    setImageVisibility(false)
   }
 
   return (
@@ -15,7 +21,14 @@ const Card = ({ children, ...pageProps }) => {
         className={`card ${center && 'center'} ${url && 'clickable'}`}
         onClick={() => goTo(url)}
       >
-        {image && <img className='headerImage' src={image} alt={title} />}
+        {image && imageVisibility && (
+          <img
+            className='headerImage'
+            src={image}
+            alt={title}
+            onError={imgSrcErrorHandler}
+          />
+        )}
         {title && <h2 className='title'>{title}</h2>}
 
         {children}
