ff433037c48251ce8057486a1d269dbc23f457a3
Felipe Oliveira <felpd3v@gmail.com>
2022-06-30 17:03:42 -0300

    Add info to the About Me section

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,8 +1,10 @@
+import { AboutMeSection } from '../../sections/AboutMe'
 import { WelcomeSection } from '../../sections/Welcome'
 import './styles.scss'
 
 export const LandingPage = () => (
 	<div className='landing-page'>
 		<WelcomeSection />
+		<AboutMeSection />
 	</div>
 )
---
+++ src/sections/AboutMe/index.js
@@ -0,0 +1,50 @@
+import './styles.scss'
+
+export const AboutMeSection = () => (
+	<section className='about-me'>
+		<header>
+			<h3>About Me</h3>
+			<h4>about me, who am I, etc</h4>
+		</header>
+
+		<main>
+			<div className='card'>
+				<img className='avatar' src='images/me.jpg' alt='black and white me' />
+			</div>
+
+			<div className='card'>
+				<p>
+					I need to start talking that I am self-taught and I really discovered
+					that when I wrote my first line of code when I was 8 years old.
+				</p>
+			</div>
+
+			<div className='card'>
+				<p>
+					I really love challenges and I think they are the best way to improve
+					my professional skills.
+					<br />
+					It's part of my job to spread my knowledge around the team, I'm so
+					comfortable to teach everybody about things I know.
+					<br />
+					In my 6 year career I learned how to work in different environments,
+					from under pressure to that more flexible.
+				</p>
+			</div>
+
+			<div className='card'>
+				<p>
+					I'm very open to suggestions like starting new things or solutions.
+					Learning a new technology, programming language or a product really
+					makes me more passionate about the job. I really love documentations,
+					I like to write from the technical ones to the product and context
+					docs. I'm very sociable and I like to talk in public.
+					<br />
+					All my communication skills are advanced because I always like to
+					express myself. My career wishes are always connected to learning
+					about companies products and contributing with technology.
+				</p>
+			</div>
+		</main>
+	</section>
+)
